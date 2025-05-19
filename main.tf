c45e5d5fe5f730b1f15d1eb415cf56bd011babf9
Shay Tsabar <shaitsabar@gmail.com>
2025-05-19 21:06:41 +0300

    add vn file

--- main.tf
+++ main.tf
@@ -4,6 +4,18 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
 }
 
+resource "azurerm_user_assigned_identity" "aks_uami" {
+  name                = "aks-identity"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_role_assignment" "aks_route_table_access" {
+  principal_id         = azurerm_user_assigned_identity.aks_uami.principal_id
+  role_definition_name = "Contributor"
+  scope                = azurerm_route_table.private_rt.id
+}
+
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
   name                = var.cluster_name
@@ -11,13 +23,16 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   dns_prefix          = var.dns_prefix
 
   identity {
-    type = "SystemAssigned"
+    type                      = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.aks_uami.id]
   }
 
+
   default_node_pool {
-    name       = "agentpool"
-    vm_size    = var.vm_size
-    node_count = var.node_count
+    name           = "agentpool"
+    vm_size        = var.vm_size
+    node_count     = var.node_count
+    vnet_subnet_id = azurerm_subnet.private.id
   }
   linux_profile {
     admin_username = var.username
@@ -27,7 +42,15 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     }
   }
   network_profile {
-    network_plugin    = "kubenet"
-    load_balancer_sku = "standard"
+  network_plugin      = "kubenet"
+  load_balancer_sku   = "standard"
+  service_cidr        = "10.240.0.0/16"
+  dns_service_ip      = "10.240.0.10"
+  outbound_type       = "userDefinedRouting"
   }
+
+  depends_on = [
+    azurerm_subnet_route_table_association.private_assoc
+  ]
+
 }
---
+++ vn.tf
@@ -0,0 +1,67 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = "public-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = "private-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_nat_gateway" "nat" {
+  name                = "nat-gateway"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "Standard"
+}
+
+resource "azurerm_public_ip" "nat_ip" {
+  name                = "nat-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "nat_assoc" {
+  nat_gateway_id       = azurerm_nat_gateway.nat.id
+  public_ip_address_id = azurerm_public_ip.nat_ip.id
+}
+
+
+resource "azurerm_subnet_nat_gateway_association" "private_nat" {
+  subnet_id      = azurerm_subnet.private.id
+  nat_gateway_id = azurerm_nat_gateway.nat.id
+}
+
+
+resource "azurerm_route_table" "private_rt" {
+  name                = "private-route-table"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet_route_table_association" "private_assoc" {
+  subnet_id      = azurerm_subnet.private.id
+  route_table_id = azurerm_route_table.private_rt.id
+}
+
+resource "azurerm_route" "default_internet" {
+  name                   = "default-to-internet"
+  resource_group_name    = azurerm_resource_group.rg.name
+  route_table_name       = azurerm_route_table.private_rt.name
+  address_prefix         = "0.0.0.0/0"
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = "0.0.0.0"
+}
